40e87b14656a56958c8bfb431eca319d1ed2ec0e
Zack Teddy <zackteddy39@gmail.com>
2024-01-03 02:03:04 +0800

    element details wip

--- components/layouts/LayoutBlueprintElementDetails.tsx
+++ components/layouts/LayoutBlueprintElementDetails.tsx
@@ -1,11 +1,46 @@
+import { useAppSelector } from '@/redux/hooks'
 import React from 'react'
+import { Label } from '../ui/label'
+import { Input } from '../ui/input'
 
 const LayoutBlueprintElementDetails = () => {
 
-    
+    const selectedElementIndex = useAppSelector((state) => state.layout.selectedElement)
+    const selectedElement = useAppSelector(
+        (state) => state.layout.blueprint[state.layout.blueprint.findIndex(
+            (element) => element.id === selectedElementIndex
+        )]
+    )
+
+    console.log('selectedElement', selectedElement)
 
     return (
-        <div>LayoutBlueprintElementDetails</div>
+        <div className='border-l pl-3'>
+            <Label>Element Details</Label>
+            <div className='border rounded p-2 mt-1 mb-1'>
+                <p className="text-xs text-muted-foreground">
+                    <strong>Element ID: </strong> {selectedElement?.id ? selectedElement.id : "" }
+                </p>
+                <p className="text-xs text-muted-foreground">
+                    <strong>Element Shape: </strong> {selectedElement?.shape ? selectedElement.shape : "" }
+                </p>
+            </div>
+            <Label>Element Variables</Label>
+            <div className='border rounded p-2 mt-1 mb-1'>
+                <div className='mb-1'>
+                    <Label htmlFor="alias">Alias Name</Label>
+                    <Input id="alias" placeholder=''></Input>
+                </div>
+                <div className='mb-1'>
+                    <Label htmlFor="text">Display Text</Label>
+                    <Input id="text" placeholder=''></Input>
+                </div>
+                <div className='mb-1'>
+                    <Label htmlFor="alias">Display Color</Label>
+                    <Input id="alias" placeholder=''></Input>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- components/layouts/ShapeElement.tsx
+++ components/layouts/ShapeElement.tsx
@@ -1,7 +1,7 @@
 import { ShapeItem } from '@/utils/types'
 import { Transformer } from 'konva/lib/shapes/Transformer';
 import React, { Dispatch, LegacyRef, SetStateAction } from 'react'
-import { Transformer as KonvaTransformer } from 'react-konva';
+import { Group, Transformer as KonvaTransformer, Text } from 'react-konva';
 
 interface ShapeElementProps {
     props: ShapeItem;
@@ -27,51 +27,56 @@ const ShapeElement = ({props, isSelected, onSelect, onChange, spacing: SPACING}:
 
     return (
         <React.Fragment>
-            <Shape 
-                key={props?.id}
-                ref={shapeRef}
-                draggable
-                onClick={onSelect}
-                onTap={onSelect}
-                onDragEnd={(e: any) => {
-                    e.target.to({
-                        x: Math.round(e.target.x() / SPACING) * SPACING,
-                        y: Math.round(e.target.y() / SPACING) * SPACING
-                    });
-                    onChange({
-                        ...props,
-                        x: Math.round(e.target.x() / SPACING) * SPACING,
-                        y: Math.round(e.target.y() / SPACING) * SPACING
-                    })
-                }}
-                onTransformEnd={(e: any) => {
-                    // transformer is changing scale of the node
-                    // and NOT its width or height
-                    // but in the store we have only width and height
-                    // to match the data better we will reset scale on transform end
-                    const node = shapeRef.current;
-                    const scaleX = node.scaleX();
-                    const scaleY = node.scaleY();
-                    // we will reset it back
-                    node.scaleX(1);
-                    node.scaleY(1);
-                    onChange({
-                        ...props,
-                        x: node.x(),
-                        y: node.y(),
-                        width: Math.round(Math.max(5, node.width() * scaleX) / SPACING) * SPACING,
-                        height: Math.round(Math.max(5, node.height() * scaleY) / SPACING) * SPACING,
-                    });
-                }}
-                { ...props }
-                {
-                    ...{
-                        x: Math.round(props.x / SPACING) * SPACING,
-                        y: Math.round(props.y / SPACING) * SPACING
+            <Group>
+                <Shape 
+                    key={props?.id}
+                    ref={shapeRef}
+                    draggable
+                    onClick={onSelect}
+                    onTap={onSelect}
+                    onDragEnd={(e: any) => {
+                        e.target.to({
+                            x: Math.round(e.target.x() / SPACING) * SPACING,
+                            y: Math.round(e.target.y() / SPACING) * SPACING
+                        });
+                        onChange({
+                            ...props,
+                            x: Math.round(e.target.x() / SPACING) * SPACING,
+                            y: Math.round(e.target.y() / SPACING) * SPACING
+                        })
+                    }}
+                    onTransformEnd={(e: any) => {
+                        // transformer is changing scale of the node
+                        // and NOT its width or height
+                        // but in the store we have only width and height
+                        // to match the data better we will reset scale on transform end
+                        const node = shapeRef.current;
+                        const scaleX = node.scaleX();
+                        const scaleY = node.scaleY();
+                        // we will reset it back
+                        node.scaleX(1);
+                        node.scaleY(1);
+                        onChange({
+                            ...props,
+                            x: node.x(),
+                            y: node.y(),
+                            width: Math.round(Math.max(5, node.width() * scaleX) / SPACING) * SPACING,
+                            height: Math.round(Math.max(5, node.height() * scaleY) / SPACING) * SPACING,
+                        });
+                    }}
+                    { ...props }
+                    {
+                        ...{
+                            x: Math.round(props.x / SPACING) * SPACING,
+                            y: Math.round(props.y / SPACING) * SPACING
+                        }
                     }
-                }
 
-            />
+                />
+                <Text>
+
+                </Text>
+            </Group>
             {
                 isSelected && (
                     <KonvaTransformer
